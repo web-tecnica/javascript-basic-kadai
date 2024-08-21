d59be2f1ec56836b160293c380910811a364f8d7
web-tecnica <info@web-tecnica.com>
2024-08-21 11:05:56 +0900

    square.jsを修正

--- kadai_014/square.js
+++ kadai_014/square.js
@@ -1,6 +1,8 @@
-// 宣言
-const square = (i) =>{
-  console.log(i*i);
+// 関数宣言
+const square = (num) =>{
+  return num*num; // ここで計算結果が戻り値として返される
 }
-square(10);
+
+let result = square(10); // 10を渡す
+console.log(result); // コンソールに出力
 
